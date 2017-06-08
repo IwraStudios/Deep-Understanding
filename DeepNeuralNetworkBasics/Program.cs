890742b33551d49961e9ed6873c62d0dc34dd8fd
IwraStudios <IwraStudios@users.noreply.github.com>
2017-06-08 09:43:29 +0200

    Comments and code
    Added a few comment to generate node function and some other function
    "Finished" Generate_Nodes code

--- DeepNeuralNetworkBasics/Program.cs
+++ DeepNeuralNetworkBasics/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace DeepNeuralNetworkBasics
 {
     //Here we are going to make the "basic fully connected neural network" with a sigmoid activation function
@@ -10,10 +11,15 @@ namespace DeepNeuralNetworkBasics
     {
         static void Main(string[] args)
         {
+        
+            List<uint> NodeLayout = new List<uint>();
+            //TODO: make easy (test) node layout
             NetNodeContainer Net = new NetNodeContainer();
+            Net.Generate_Nodes();
 
         }
 
+        //Single network
         class NetContainer
         {
             uint inputAmount = 2;
@@ -29,53 +35,79 @@ namespace DeepNeuralNetworkBasics
             }
 
 
-            void Generate_Nodes()
+            public void Generate_Nodes()
             {
+                //Generation Structure
+                //InputLayer(0)
+                //HiddenLayers(1-x)
+                //OutputLayer(x+1)
+                //Generation Structure/
+            
+                //Random class for initialization
+                //We won't be using "special" wieght initialization
+                //Just random numbers
+                Random r = new Random();
+            
+                //Ouput network
                 List<List<NetNodeContainer>> Out = new List<List<NetNodeContainer>>();
+                //Single Layer
                 List<NetNodeContainer> TMP = new List<NetNodeContainer>();
+                
+                ///InputNodes
                 for(int i = 0; i < inputAmount; i++) //Generate Output Nodes
                 {
-                    TMP.Add(new NetNodeContainer() { layer = 0,
-                        Weights = Generate_RND_Weights(NodesLayers.First()) //Generate x amount of Random weights(x = the amount of nodes that follow)
+                    TMP.Add(new NetNodeContainer() { layer = 0, //InputLayer(0)
+                        Weights = r.Next()//Make weights a radom number
                     });
                 }
-                Out.Add(TMP);
-                TMP = new List<NetNodeContainer>();
+                Out.Add(TMP); //Add inputs to network
+                ///InputNodes/
+                
+                //Generate Hidden Nodes
+                for (int j = 0; j< NodesLayers.Count; j++){//for amount of layers
+                    TMP = new List<NetNodeContainer>(); //Make empty
+                    for (int i = 0; i < OutputAmount; i++)//Nodes of each layer
+                    {
+                        TMP.Add(new NetNodeContainer() { layer = (uint)NodesLayers.Count+1, //HiddenLayers(1-x)
+                            Weights = r.Next()//Make weights a radom number
+                        });
+                    }
+                    Out.Add(TMP);
+                }
+                
+                //Generate Hidden Nodes/
+                
+                
+                ///OutputNodes
+                TMP = new List<NetNodeContainer>(); //Make empty
                 for (int i = 0; i < OutputAmount; i++) //Genereate Output Nodes
                 {
-                    TMP.Add(new NetNodeContainer() { layer = (uint)NodesLayers.Count,
-                        Weights = null
+                    TMP.Add(new NetNodeContainer() { layer = (uint)NodesLayers.Count+1, //OutputLayer(x+1)
+                        Weights = r.Next()//Make weights a radom number
                     });
                 }
                 Out.Add(TMP);
+                ///OutputNodes/
 
-            }
-
-            List<double> Generate_RND_Weights(uint amount)
-            {
-                List<double> Out = new List<double>();
-                Random r = new Random();
-                for(int i = 0; i < amount; i++)
-                {
-                    Out.Add(r.Next());
-                }
-                return Out;
             }
             
         }
         
+        //Single Node
         class NetNodeContainer
         {
            public uint layer;
            public List<double> Weights; //The way neural nets function is by changing weights from and to each node
            public double LastOutput;
         }
-
+        
+        //Generic Sigmoid function
         static double Sigmoid(double inp)
         {
             return (1 / (1 + Math.Exp(inp)));
         }
-
+        
+        //Generic Sum function
         static double SUM(List<double> inp)
         {
             double outp = 0;
